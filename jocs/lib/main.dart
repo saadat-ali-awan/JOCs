3653235020aa0c706ee8d0ea898574571016694e
Saadat123456 <saadatlucky313@gmail.com>
2021-12-24 22:18:38 +0500

    Cleared all files and Added comments to main

--- jocs/lib/main.dart
+++ jocs/lib/main.dart
@@ -6,48 +6,20 @@ import 'Registration/login.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
+
+  // First to make sure Flutter is Initialized WidgetsFlutterBinding.ensureInitialized() will be called
+  // It is required for Firebase Initialization
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Firebase is initialized using firebase_options.dart
+  // firebase_options.dart is generated automatically by flutterfire configure command
+  // It contains all the details to connect with Firebase
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
+
+  // Entry Point For App
+  // Firebase will go to Login Page initially
   runApp(const GetMaterialApp(home: Login()));
-}
-
-class Controller extends GetxController{
-  var count = 0.obs;
-  increment() => count++;
-}
-
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
-
-
-  @override
-  Widget build(context) {
-
-    // Instantiate your class using Get.put() to make it available for all "child" routes there.
-    final Controller c = Get.put(Controller());
-
-    return Scaffold(
-      // Use Obx(()=> to update Text() whenever count is changed.
-        appBar: AppBar(title: Obx(() => Text("Clicks: ${c.count}"))),
-
-        // Replace the 8 lines Navigator.push by a simple Get.to(). You don't need context
-        body: Center(child: ElevatedButton(
-            child: Text("Go to Other"), onPressed: () => Get.to(Other()))),
-        floatingActionButton:
-        FloatingActionButton(child: Icon(Icons.add), onPressed: c.increment));
-  }
-}
-
-class Other extends StatelessWidget {
-  // You can ask Get to find a Controller that is being used by another page and redirect you to it.
-  final Controller c = Get.find();
-
-  @override
-  Widget build(context){
-    // Access the updated count variable
-    return Scaffold(body: Center(child: Text("${c.count}")));
-  }
 }
