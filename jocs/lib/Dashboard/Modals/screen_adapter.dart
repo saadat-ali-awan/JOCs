46e3579b8eb55e7b43f8f0d487cfe9d1a986168e
Saadat123456 <saadatlucky313@gmail.com>
2022-01-07 03:05:42 +0500

    Add General Adapter for all screens. ( screen_adapter.dart )

---
+++ jocs/lib/Dashboard/Modals/screen_adapter.dart
@@ -0,0 +1,46 @@
+import 'package:get/get.dart';
+
+class ScreenAdapter {
+  RxInt currentPage = 1.obs;
+  RxInt lastId = 0.obs;
+  int articlesOnOnePage = 10;
+  RxList adapterData = RxList();
+  String screenName = "";
+
+  ScreenAdapter(this.screenName);
+
+  getTicketsData(firebaseController, {String filter="", previous=false}) async {
+    if (previous){
+
+    }
+    if (adapterData.length < currentPage.value ) {
+      var data = await firebaseController.getData(screenName, currentPage.value, articlesOnOnePage, filter: filter);
+      if (data.docs.length == 0){
+        currentPage -= 1;
+      }else {
+        adapterData.add([]);
+        data.docs.forEach((res) {
+          adapterData[currentPage.value-1].add(res);
+        });
+      }
+    }
+  }
+
+  getNextPage(firebaseController){
+    currentPage.value+=1;
+    getTicketsData(firebaseController, filter: adapterData[currentPage.value-2][articlesOnOnePage-1]["time"]);
+  }
+  getPreviousPage(firebaseController){
+    currentPage.value-=1;
+    if (currentPage.value < 1){
+      currentPage.value = 1;
+    }
+  }
+
+  refreshCurrentPage(firebaseController) async{
+    adapterData = RxList();
+    currentPage.value = 1;
+    lastId.value = await firebaseController.getLastId(screenName);
+    getTicketsData(firebaseController);
+  }
+}
